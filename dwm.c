41b5713754be21354dd160050654fb82a5c679c6
Peter Hofmann <scminsecure@uninformativ.de>
2012-12-17 09:28:23 +0100

    Revert "noborder: Removed additional window resizing."
    This reverts commit 91d9d97e626c39f74aaa3bb9b92a731fed480eb7.
    
    Conflicts:
    	dwm.c
    
    Sometimes, the border is not removed from windows. Can't tell exactly
    why or when. Let's try reverting this commit.

--- dwm.c
+++ dwm.c
@@ -1848,7 +1848,7 @@ textnw(const char *text, unsigned int len) {
 
 void
 tile(Monitor *m) {
-	unsigned int i, n, h, mw, my, ty;
+	unsigned int i, n, h, mw, my, ty, r;
 	Client *c;
 
 	for(n = 0, c = nexttiled(m->clients); c; c = nexttiled(c->next), n++);
@@ -1859,27 +1859,33 @@ tile(Monitor *m) {
 		mw = m->nmaster ? m->ww * m->mfact : 0;
 	else
 		mw = m->ww;
-	for(i = my = ty = 0, c = nexttiled(m->clients); c; c = nexttiled(c->next), i++) {
+	for(i = my = ty = 0, c = nexttiled(m->clients); c; c = nexttiled(c->next), i++, r = 0) {
 		if(n == 1) {
 			if (c->bw) {
 				/* remove border when only one window is on the current tag */
 				c->oldbw = c->bw;
 				c->bw = 0;
+				r = 1;
 			}
 		}
 		else if(!c->bw && c->oldbw) {
 			/* restore border when more than one window is displayed */
 			c->bw = c->oldbw;
 			c->oldbw = 0;
+			r = 1;
 		}
 		if(i < m->nmaster) {
 			h = (m->wh - my) / (MIN(n, m->nmaster) - i);
 			resize(c, m->wx, m->wy + my, mw - (2*c->bw), h - (2*c->bw), False);
+			if(r)
+				resizeclient(c, m->wx, m->wy + my, mw - (2*c->bw), h - (2*c->bw));
 			my += HEIGHT(c);
 		}
 		else {
 			h = (m->wh - ty) / (n - i);
 			resize(c, m->wx + mw, m->wy + ty, m->ww - mw - (2*c->bw), h - (2*c->bw), False);
+			if(r)
+				resizeclient(c, m->wx + mw, m->wy + ty, m->ww - mw - (2*c->bw), h - (2*c->bw));
 			ty += HEIGHT(c);
 		}
 	}
