1e4e75b70b43337240f3abfa7bc9a9a6c1977602
Peter Hofmann <scm@uninformativ.de>
2014-06-08 19:03:49 +0200

    Makefile: Optimize for speed rather than size
    I don't care if the binary is 6kb larger...

--- config.mk
+++ config.mk
@@ -20,7 +20,7 @@ LIBS = -L${X11LIB} -lX11 ${XINERAMALIBS} -lXfixes
 
 # flags
 CPPFLAGS = -D_BSD_SOURCE -D_POSIX_C_SOURCE=2 -DVERSION=\"${VERSION}\" -DYEAR=\"${YEAR}\" ${XINERAMAFLAGS}
-CFLAGS   = -std=c99 -pedantic -Wall -Wno-deprecated-declarations -Os ${INCS} ${CPPFLAGS}
+CFLAGS   = -std=c99 -pedantic -Wall -Wno-deprecated-declarations -O2 ${INCS} ${CPPFLAGS}
 LDFLAGS  = -s ${LIBS}
 
 # compiler and linker
